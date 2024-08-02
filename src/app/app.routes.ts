a72d0081769049c7649a863d021edfc69215e2db
Florian Dima <dflorian251@gmail.com>
2024-08-02 14:33:43 +0300

    Welcome Page added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    {path: '', component: WelcomeComponent},
-    {path: 'products', component: ProductListComponent},
+    { path: 'products', component: ProductListComponent},
+    { path: '', component: WelcomeComponent},
+    { path: '**', component: PageNotFoundComponent},
 ];
 
